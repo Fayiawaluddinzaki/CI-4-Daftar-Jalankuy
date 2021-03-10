9910121d5270ffeefb2a90f0ab5aa3338cc01aaa
Fayiawaluddinzaki <zakiawaluddin@gmail.com>
2021-03-10 21:55:03 +0700

    First commit

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Auth extends BaseController
+{
+	public function index()
+	{
+		//
+		return view('auth/login');
+	}
+
+	public function register()
+	{
+		return view('auth/register');
+	}
+
+	public function logout()
+	{
+		return view('home/index');
+	}
+}
